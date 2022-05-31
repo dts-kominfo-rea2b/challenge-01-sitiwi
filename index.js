aa4c763a9763197c152dbe9abb6a30cdc2f1c55a
sitiwi <sitipratiwi95@gmail.com>
2022-05-31 21:52:17 +0800

    memperbarui data Array Set

--- index.js
+++ index.js
@@ -1,66 +1,67 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
+const colorUserOne = new Set(["Yellow", "Pink", "White","Purple"]);
+const colorUserTwo = new Set(["Blue","Black","Grey"]);
+const restaurantOne = new Set(["Bento","Sushi","Pancake","Eggy","Tempura","Bento","Padang","Tteok","Sushi","Sushi"]);
+const restaurantTwo = new Set(["Tempura","Bento","Sushi","Pancake","Padang","Katsu","Geprek","Eggy"]);
+
 const firstUser = {
     name : "Monica",
     gender : "Female",
     age : 17,
     email : "monica@dingdong.com",
-    favoriteColor : new Set(["Yellow","Pink","White","Purple"]),
+    favoriteColor : Array.from(colorUserOne),
     isHavePet : true,
     education : [
-        [   
-            "name:", "SD 01",
-            "city:", "Jakarta",
-            "graduate:", "2016"
-        ],[
-            "name:", "SMP 02",
-            "city:", "Jakarta",
-            "graduate:", "2019"
-        ],[
-            "name:", "SMA 03",
-            "city:","Tangerang",
-            "graduate:", "0",
-        ]
+        {   
+            name: "SD 01",
+            city: "Jakarta",
+            graduate: "2016"
+        },{
+            name: "SMP 02",
+            city: "Jakarta",
+            graduate: "2019"
+        },{
+            name: "SMA 03",
+            city: "Tangerang"
+        }
     ],
     
-    favoriteRestaurant : new Set(["Bento","Sushi","Pancake","Eggy","Tempura","Bento","Padang","Tteok","Sushi","Sushi"]),
+    favoriteRestaurant :Array.from(restaurantOne)
 };
-
-
 
 const secondUser = {
     name : "Wendy",
     gender : "Male",
     age : 23,
     email : "wendy@dingdong.com",
-    favoriteColor : new Set(["Blue","Black","Grey"]),
+    favoriteColor : Array.from(colorUserTwo),
     isHavePet : false,
     education : [
-        [   "name", "SD 02", 
-            "city", "Jakarta", 
-            "graduate","2010"
-        ],[
-            "name", "SMP 03", 
-            "city", "Bogor", 
-            "graduate", "2013"
-        ],[ "name", "SMA 01",
-            "city", "Surabaya",
-            "graduate", "2016",
-        ],[ "name", "Universitas Maju",
-            "city", "Tangerang",
-            "graduate", "null"
-        ]
+        {   name: "SD 02", 
+            city:"Jakarta", 
+            graduate :"2010"
+        },{
+            name: "SMP 03", 
+            city: "Bogor", 
+            graduate: "2013"
+        },{ name: "SMA 01",
+            city: "Surabaya",
+            graduate:"2016",
+        },{ name: "Universitas Maju",
+            city:"Tangerang",
+        }
     ],
-    favoriteRestaurant : new Set(["Tempura","Bento","Sushi","Pancake","Padang","Katsu","Geprek","Eggy"]),
+    favoriteRestaurant : Array.from(restaurantTwo),
 };
     
     
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
-const users = [firstUser,secondUser];
-    // users.push(firstUser);
-    // users.push(secondUser);
+const users = [];
+    users.push(firstUser);
+    users.push(secondUser);
 
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
